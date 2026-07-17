d066c919f09930b35b346513f1ba46a302fc7dfc
Mauricio Dioli <madioli26@hotmail.com>
2026-07-17 16:52:25 +0200

    Expone Flask en puerto 8300

--- src/app.py
+++ src/app.py
@@ -125,4 +125,4 @@ def test_db_connection():
 test_db_connection()
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8100, debug=True, use_reloader=False)
+    app.run(host="0.0.0.0", port=8300, debug=True, use_reloader=False)
